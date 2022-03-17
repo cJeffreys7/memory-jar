281fdc605f974239a5eac8c0ce72b54f30f78a70
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-03-17 11:08:16 -0500

    refactor token log

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -6,8 +6,7 @@ const setToken = (token) => {
 
 const getToken = () => {
     let token = localStorage.getItem('token') || null;
-    console.log('JSON Web Token: ', token);
-    console.log('JSON Web Token: ', token ? JSON.parse(Buffer.from(token.split('.')[1], 'base64')) : 'NULL');
+    console.log('JSON Web Token: ', token !== null ? JSON.parse(Buffer.from(token.split('.')[1], 'base64')) : 'NULL');
     if (token !== null) {
         const payload = JSON.parse(Buffer.from(token.split('.')[1], 'base64'));
         // 9000 seconds is 2.5 hours
