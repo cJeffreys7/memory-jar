54c80b1526a4500e0f9819e1e7fa419d2a65b7b8
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-03-17 11:22:56 -0500

    token log testing

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -7,21 +7,14 @@ const setToken = (token) => {
 const getToken = () => {
     let token = localStorage.getItem('token') || null;
     console.log('TOKEN: ', token);
-    if (token === null) {
-        console.log('TOKEN IS NULL');
-    }
-    if (!!token === false) {
-        console.log('TOKEN IS FALSE');
-    }
-    if (!token) {
-        console.log('TOKEN IS TRUE');
-    }
+    console.log('TOKEN BANG: ', !token);
+    console.log('TOKEN DOUBLE BANG: ', !!token);
     if (token !== null) {
         const payload = JSON.parse(Buffer.from(token.split('.')[1], 'base64'));
         // 9000 seconds is 2.5 hours
         // console.log('Token time: ', payload.exp + 60000);
         // console.log('Current time: ', Date.now() / 1000);
-        if ((payload.exp + 600000) < (Date.now() / 1000)) {
+        if (payload.exp < (Date.now() / 1000)) {
             console.log('Token expired');
             localStorage.removeItem('token');
             token = null;
