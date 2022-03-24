fc80b2b5dc7a99deb4e6b8133f6215f5395ccd7d
Chris Jeffreys <chrisbjeffreys@gmail.com>
2022-03-24 10:57:44 -0500

    refactor image skeleton

--- src/components/MemoryJarPreview/index.jsx
+++ src/components/MemoryJarPreview/index.jsx
@@ -11,7 +11,10 @@ import * as memoryJarService from '../../services/memoryJarService';
 import './styles.scss'
 
 const MemoryJarPreview = ({ jar }) => {
-    const memoryPreviews = memoryJarService.mapMemories(jar.jarId, jar.memories);
+    let memoryPreviews;
+    if (jar.memories) {
+        memoryPreviews = memoryJarService.mapMemories(jar.jarId, jar.memories);
+    }
 
     return (
         <div className='memory-jar-preview-wrapper'>
@@ -19,13 +22,19 @@ const MemoryJarPreview = ({ jar }) => {
                 <img className='preview-icon' src={MemoryJarIcon} alt="Memory Jar Preview" />
                 <div className='preview-images'>
                     <div id='preview-image-1' className='preview-image'>
-                        <MemoryImageFrame memory={memoryPreviews[0]} />
+                        {memoryPreviews?.length > 0 &&
+                            <MemoryImageFrame memory={memoryPreviews[0]} />
+                        }
                     </div>
                     <div id='preview-image-2' className='preview-image'>
-                        <MemoryImageFrame memory={memoryPreviews[1]} />
+                        {memoryPreviews?.length > 2 && 
+                            <MemoryImageFrame memory={memoryPreviews[2]} />
+                        }
                     </div>
                     <div id='preview-image-3' className='preview-image'>
-                        <MemoryImageFrame memory={memoryPreviews[2]} />
+                        {memoryPreviews?.length > 1 && 
+                            <MemoryImageFrame memory={memoryPreviews[1]} />
+                        }
                     </div>
                 </div>
             </Link>
